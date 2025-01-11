877b57ceed6fe943ccd30d4e36b2145b879ab09d
ashlen <thesadru@gmail.com>
2025-01-11 15:46:12 +0100

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+In `releasever.json` you can find a mapping of character id to the first version they appeared in.
+
+Notes:
+- There is no data for characters before `19-12-28-08-02-55-e463c0` (Few days before Snowsant's release). PR to fix this would be appreciated.
+- Release version timestamp does not refer to when the version is released to players. Typically the character is only available several days after the timestamp.
+- `char_008_owl` and `char_279_knit3` are not playable characters and will not show up in `character_table.json`
---
+++ script.py
@@ -0,0 +1,39 @@
+import urllib.error
+import urllib.request
+import json
+
+def fetch_current_version() -> str:
+    with urllib.request.urlopen(f"https://ak-conf.hypergryph.com/config/prod/official/Android/version") as response:
+        return json.loads(response.read())["resVersion"]
+
+
+def fetch_hot_update_list(version: str):
+    with urllib.request.urlopen(f"https://ak.hycdn.cn/assetbundle/official/Android/assets/{version}/hot_update_list.json") as response:
+        return json.loads(response.read())
+
+def get_characters_in_version(version: str) -> list[str]:
+    try:
+        hol = fetch_hot_update_list(version)
+    except urllib.error.HTTPError as e:
+        if e.code == 404:
+            return []
+        raise
+
+    characters = []
+    for info in hol["abInfos"]:
+        if info["name"].startswith("charpack/char_"):
+            characters.append(info["name"].split("/", 1)[1].split(".", 1)[0])
+    
+    return characters
+
+
+with open("releasever.json") as file:
+    releasever: dict[str, str] = json.load(file)
+
+current_version = fetch_current_version()
+for character in get_characters_in_version(current_version):
+    if character not in releasever:
+        releasever[character] = current_version
+
+with open("releasever.json", "w") as file:
+    json.dump(releasever, file, indent=4)
